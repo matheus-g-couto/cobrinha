1e0224d9fa15fc5c683313e3c1649a56c7530dea
WagnerACV <40211958+WagnerACV@users.noreply.github.com>
2018-09-30 19:37:12 -0300

    Update js-index.js

--- js-index.js
+++ js-index.js
@@ -2,6 +2,10 @@ $("#abrir-modal").click(logar);
 $("#inscrever").click(inscrever);
 $(".closer").click(logar);
 $(".closer2").click(inscrever);
+$(".componentes").click(componentes);
+$(".closer3").click(componentes);
+$(".fontes").click(fontes);
+$(".closer4").click(fontes);
 
 function logar(){
   $("#logar-mensagem").toggleClass("modal-visivel");
@@ -17,4 +21,22 @@ function inscrever(){
   $("#logar-conteudo").toggleClass("trans-conteudo");
   $("#inscrever-mensagem").toggleClass("modal-visivel");
   $("#inscrever-conteudo").toggleClass("trans-conteudo");
+}
+
+function componentes(){
+  $("#componentes").toggleClass("modal-visivel");
+  $(".central").toggleClass("desfoque");
+  $(".minhoca").toggleClass("desfoque");
+  $(".componentes").toggleClass("desfoque");
+  $(".fontes").toggleClass("desfoque");
+  $("#componentes-conteudo").toggleClass("trans-conteudo");
+}
+
+function fontes(){
+  $("#fontes-mensagem").toggleClass("modal-visivel");
+  $(".central").toggleClass("desfoque");
+  $(".minhoca").toggleClass("desfoque");
+  $(".componentes").toggleClass("desfoque");
+  $(".fontes").toggleClass("desfoque");
+  $("#fontes-conteudo").toggleClass("trans-conteudo");
 }
