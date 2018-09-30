dba4af54e82f3582c1f3ae4034b8cd91deb739f3
Ruan-ceara <40470511+Ruan-ceara@users.noreply.github.com>
2018-09-30 17:47:35 -0300

    Add files via upload

--- storage.js
+++ storage.js
@@ -4,10 +4,12 @@ let UsuarioInsEl = document.getElementById('UsuarioIns');
 let SenhaInsEl = document.getElementById('SenhaIns');
 let CSenhaInsEl = document.getElementById('CSenhaIns');
 let EnviarIns = document.getElementById('enviar2');
+let EnviarLog = document.getElementById('enviar1');
 let x = localStorage.getItem('senha', SenhaIns);
 let y = localStorage.getItem('confirmacaoSenha', CSenhaIns);
-
+let z = localStorage.getItem('usuario',UsuarioIns);
 EnviarIns.addEventListener('click',salvaLogin);
+EnviarLog.addEventListener('click',entraJogo);
 
 function salvaLogin(){
 let UsuarioIns = UsuarioInsEl.value;
@@ -18,14 +20,17 @@ localStorage.setItem('senha', SenhaIns);
 localStorage.setItem('confirmacaoSenha', CSenhaIns);
 x =  localStorage.getItem('senha', SenhaIns);
 y = localStorage.getItem('confirmacaoSenha', CSenhaIns);
+z = localStorage.getItem('usuario',UsuarioIns);
 document.getElementById('senhaIncorreta').innerHTML = '';
 if(x == y){
   document.getElementById('senhaIncorreta').innerHTML = "<p style = 'color:green'>Conta criada,clique em voltar.</p>";
 }else{document.getElementById('senhaIncorreta').innerHTML = "<p style = 'color:red'>As senhas não são idênticas.</p>";}
 }
-function inscrever(){
-  $('#logar-mensagem').toggleClass('modal-visivel');
-  $('#logar-conteudo').toggleClass('trans-conteudo');
-  $('#inscrever-mensagem').toggleClass('modal-visivel');
-  $('#inscrever-conteudo').toggleClass('trans-conteudo');
+
+function entraJogo(){
+  document.getElementById('enviar1').innerHTML = 'Enviar';
+  if(UsuarioLogEl.value === z && SenhaLogEl.value === x && SenhaLogEl.value === y){
+    document.getElementById('enviar1').innerHTML = "<a href='jogo.html'style='margin: 0; text-decoration:none' button type='button' id='enviar1' class='input-submit'>Enviar</button></a>";
+    document.getElementById('loginAprovado').innerHTML = "<p style = 'color:green'>Login aprovado,clique novamente em enviar para acessar o jogo.</p>";
+  }else{document.getElementById('loginAprovado').innerHTML = "<p style = 'color:red'>Senha/Login incorreta(o).</p>";}
 }
