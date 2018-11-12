2b5b0acc78c523e0e35aa5b882ddde5318e4c706
WagnerACV <40211958+WagnerACV@users.noreply.github.com>
2018-11-12 12:45:15 -0200

    Add files via upload

--- jogo-offline.js
+++ jogo-offline.js
@@ -59,6 +59,19 @@ function draw(){
     playlabel.draw();
 }
 
+var elem = document.querySelector("html");
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
 //define o tamanho do canvas
 function definirTamanho(){
   WIDTH = window.innerWidth;
